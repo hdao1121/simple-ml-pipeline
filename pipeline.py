8f6adc3ee19dde0b26b2656535e06e9111c33410
hdao1121 <huy@littlstar.com>
2018-06-20 18:25:29 -0400

    download data py

---
+++ pipeline.py
@@ -0,0 +1,15 @@
+import os
+import tarfile
+from six.moves import urllib
+
+HOUSING_PATH = os.path.join("datasets", "housing")
+HOUSING_URL = "https://raw.githubusercontent.com/ageron/handson-ml/master/datasets/housing/housing.tgz"
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
