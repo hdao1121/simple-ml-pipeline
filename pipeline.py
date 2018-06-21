f83f6008c6c31a19ade927cab484cca00a01eb34
hdao1121 <huy@littlstar.com>
2018-06-21 11:14:23 -0400

    explore data

--- pipeline.py
+++ pipeline.py
@@ -13,3 +13,19 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     housing_tgz = tarfile.open(tgz_path)
     housing_tgz.extractall(path=housing_path)
     housing_tgz.close()
+fetch_housing_data()
+
+import pandas as pd
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+housing = load_housing_data()
+
+def explore_data(data):
+    print(data.head())
+    print(data.info())
+    print(data["ocean_proximity"].value_counts())
+    print(data.describe())
+
+explore_data(housing)
