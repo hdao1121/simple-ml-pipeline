898a9d81835b2fc342f20fb4b5f5f475891828c5
hdao1121 <huy.dao13@gmail.com>
2018-06-25 14:39:00 -0400

    update

--- pipeline.py
+++ pipeline.py
@@ -5,11 +5,12 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import StratifiedShuffleSplit
 import numpy as np
 import pandas as pd
+from pandas.plotting import scatter_matrix
 
 HOUSING_PATH = os.path.join("datasets", "housing")
 HOUSING_URL = "https://raw.githubusercontent.com/ageron/handson-ml/master/datasets/housing/housing.tgz"
 
-def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+def fetch_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     if not os.path.isdir(housing_path):
         os.makedirs(housing_path)
     tgz_path = os.path.join(housing_path, "housing.tgz")
@@ -17,7 +18,7 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     housing_tgz = tarfile.open(tgz_path)
     housing_tgz.extractall(path=housing_path)
     housing_tgz.close()
-fetch_housing_data()
+# fetch_data()
 
 def load_housing_data(housing_path=HOUSING_PATH):
     csv_path = os.path.join(housing_path, "housing.csv")
@@ -30,21 +31,30 @@ def explore_data(data):
     print(data["ocean_proximity"].value_counts())
     print(data.describe())
 
-def visualize_data(data):
-    data.hist(bins=50, figsize=(20,15))
-    plt.show()
-
 def split_data(data):
     data["income_cat"] = np.ceil(data["median_income"]/1.5)
     data["income_cat"].where(data["income_cat"] < 5, 5.0, inplace=True)
 
     # check each category percentage
-    print(data["income_cat"].value_counts() / len(data))
+    # print(data["income_cat"].value_counts() / len(data))
 
     split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
     for train_index, test_index in split.split(data, data["income_cat"]):
         strat_train_set = data.loc[train_index]
         strat_test_set = data.loc[test_index]
+    for set in (strat_test_set, strat_train_set):
+        set.drop(["income_cat"], axis=1, inplace=True)
+    return strat_train_set, strat_test_set
 
+train_set, test_set = split_data(housing)
 
-split_data(housing)
+def visualize_data(data):
+    data.hist(bins=50,figsize=(20,15))
+    data.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4,
+              s=housing["population"]/100, label="population", c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True)
+    plt.legend()
+    # print(data.corr())
+    attributes = ["median_house_value", "median_income", "total_rooms", "housing_median_age"]
+    scatter_matrix(housing[attributes], figsize=(12,8))
+    plt.show()
+visualize_data(train_set)
